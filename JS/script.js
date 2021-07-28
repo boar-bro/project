aeaf1e721746135625d0f2aa8d04023a106301ea
boar_bro <link1705@gmail.com>
2021-07-28 20:51:59 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+//alert('Hello');
+
+//const result = confirm('Are You Here?');
+//console.log (result);
+
+
+//const answer = +prompt('Вас есть 18?', '');
+//console.log(answer + 5);
+
+
+//const answers = [];
+
+//answers[0] = prompt('Как ваше имя?', '');
+//answers[1] = prompt('Как ваша фамилия?', '');
+//answers[2] = prompt('Какой ваш возраст?', '');
+
+
+//document.write(answers);
+
+
+//const category = 'toys';
+//console.log(`https//someurl.com/${category}/5`);
+
+//const user = 'Ivan';
+//alert(`Привет, ${user}`);
+
+
+//console.log('arr' + ' - object');
+//console.log('4' + ' - object');
+
+
+//let incr = 10,
+//	decr = 10;
+
+
+////++incr;
+////--decr;
+
+//console.log(++incr);
+//console.log(--decr);
+
+//console.log(5%2);
+
+//console.log(2 + 2 * 2 !== '6');
+
+//console.log(2*4 === '8');
+//console.log(2*4 === 8);
+
+
+//const isChecked = false,
+//	isClose = false;
+
+//	console.log(isChecked || !isClose);
+
+
+
+
+
+
